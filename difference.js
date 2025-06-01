290975e9c5cfabd0ad5babcdb6d20c79faa829e3
Kabir058 <riduanulkabir01@gmail.com>
2025-06-01 10:37:26 +0600

    function call to get the difference of values

---
+++ difference.js
@@ -0,0 +1,9 @@
+function diff(x,y)
+{   
+    console.log("values are ",x,y)
+    let difference =x-y; 
+    console.log("The difference is ", difference)
+}
+let son_age= 20;
+let father_age= 55;
+diff(father_age,son_age)
--- functon2.js
+++ functon2.js
@@ -4,3 +4,8 @@ function multipli (x,y){
    console.log("The result will be:",result)
 }
 multipli(3,7);
+multipli(64,3);
+multipli(7,9);
+multipli(8,5);
+multipli(3);
+
