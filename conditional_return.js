1bf3ceb907965eba80fb3eb85608995bda738144
Kabir058 <riduanulkabir01@gmail.com>
2025-06-01 10:32:43 +0600

    function : uses of conditional return if/else

---
+++ conditional_return.js
@@ -0,0 +1,19 @@
+function number(num)
+{
+    if(num%2===0)
+        return true;
+    else{
+        return false;
+    }
+}
+// console.log(number(4))
+// console.log(number(55))
+
+function isOdd(value)
+{
+    if(value%2===0)
+         {return false;}
+    return true;
+        
+}
+console.log(isOdd(15))
