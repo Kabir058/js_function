2f0aa8edf2f6d12f90ceabb3facf32f7759e5463
Kabir058 <riduanulkabir01@gmail.com>
2025-06-01 09:58:16 +0600

    first commit

---
+++ function.js
@@ -0,0 +1,8 @@
+function fanOff(){
+    console.log('put down the phone')
+        console.log('wake up from the bed')
+            console.log("go towards the fan switch and put it off")
+        
+    
+}
+fanOff()
