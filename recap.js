45998863e00bef236553c20946fce58fd8dbbbf1
Kabir058 <riduanulkabir01@gmail.com>
2025-06-01 10:14:08 +0600

    function : recap function where used return method in different way

---
+++ recap.js
@@ -0,0 +1,11 @@
+function doMath(num1,num2)
+{
+    let add = num1+num2;
+    let subtract = num1-num2;
+    let multiply= add*subtract;
+    let division = multiply/3;
+    return division;
+}
+
+let result = doMath(10,5);
+console.log(result)
---
+++ return.js
@@ -0,0 +1,8 @@
+function tenTimes(number)
+{
+    let answer= number*10;
+    return answer;
+}
+
+let toGetReturn = tenTimes(5);
+console.log(toGetReturn)
